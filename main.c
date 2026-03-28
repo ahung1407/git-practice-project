28b9fd75cdcb246c4da480bb484e9e35b01be0c9
Practice User <user@example.com>
2026-03-28 15:26:41 +0700

    feat: Add memory calculation utility

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int calculate_memory() {
+    // Returns memory in KB
+    return 1024;
+}
+
 int main() {
-    printf("Welcome to the System Module!
-");
+    printf("Welcome to the System Module!\n");
+    int mem = calculate_memory();
+    printf("Current memory available: %dKB\n", mem);
     return 0;
 }
