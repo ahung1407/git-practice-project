2ba1a0b17cdc93721d8b24d84dfe66e04cfe432f
Practice User <user@example.com>
2026-03-29 15:08:45 +0700

    feat(driver): add initial driver implementation and gitignore

---
+++ feature_driver.c
@@ -0,0 +1,9 @@
+// This is a critical hardware driver.
+// Initial driver structure
+void init_driver() {
+    // TODO: Implement driver initialization
+}
+
+void release_driver() {
+    // Release hardware resources
+}
